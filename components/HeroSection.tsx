f33f4d16bcafc7f056a4061402ba397fc925e012
Shiv Patel <156535635+Shiv-sp@users.noreply.github.com>
2025-01-03 07:14:40 -0500

    Update HeroSection.tsx

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -70,7 +70,7 @@ const HeroSection = () => {
             {" "}related roles!
           </p>
           <a
-            href="/ShivPatel_Resume_EngineeringIntern (2).pdf"
+            href="/ShivPatel_Resume_Jan2025.pdf"
             className="text-neutral-100 font-semibold px-6 py-3 bg-green-500 rounded shadow hover:bg-teal-700"
             target="_blank"
             rel="noopener noreferrer"
